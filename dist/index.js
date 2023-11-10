c694f6fa37a0dcb0c44f27dbe2d368d8c5df34dd
Sakthivel Selvam <sakthiselvamcs@outlook.com>
2023-11-10 17:17:51 +0530

    m

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 (async function () {
-    const apiUrl = 'https://jsonplaceholder.org/posts?id=10';
+    const apiUrl = 'https://jsonplaceholder.typicode.com/comments/?_limit=15';
 
     var comments = await fetch(apiUrl)
         .then(response => {
